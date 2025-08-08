e95f1e6e47cb3aca2b374ab46e4fd92e43acab9b
hetvi1405 <8361hetvi@gmail.com>
2025-08-08 22:34:47 +0530

    second_commit

--- React_todo/src/components/TodoList.jsx
+++ React_todo/src/components/TodoList.jsx
@@ -1,20 +1,50 @@
-// src/components/TodoList.js
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { toggleTodo, deleteTodo } from '../features/todoSlice'
+import { toggleTodo, deleteTodo, updateTodo } from '../features/todoSlice'
 
 const TodoList = () => {
   const todos = useSelector(state => state.todos)
   const dispatch = useDispatch()
 
+  const [editId, setEditId] = useState(null)
+  const [editText, setEditText] = useState('')
+
+  const handleEdit = (todo) => {
+    setEditId(todo.id)
+    setEditText(todo.text)
+  }
+
+  const handleUpdate = (id) => {
+    if (editText.trim() !== '') {
+      dispatch(updateTodo({ id, newText: editText }))
+      setEditId(null)
+      setEditText('')
+    }
+  }
+
   return (
     <ul>
       {todos.map(todo => (
         <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-          <span onClick={() => dispatch(toggleTodo(todo.id))}>
-            {todo.text}
-          </span>
-          <button onClick={() => dispatch(deleteTodo(todo.id))}>x</button>
+          {editId === todo.id ? (
+            <>
+              <input
+                type="text"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+              />
+              <button onClick={() => handleUpdate(todo.id)}>Save</button>
+           
+            </>
+          ) : (
+            <>
+              <span onClick={() => dispatch(toggleTodo(todo.id))}>
+                {todo.text}
+              </span>
+              <button onClick={() => handleEdit(todo)}>Edit</button>
+              <button onClick={() => dispatch(deleteTodo(todo.id))}>x</button>
+            </>
+          )}
         </li>
       ))}
     </ul>
--- React_todo/src/features/todoSlice.js
+++ React_todo/src/features/todoSlice.js
@@ -1,4 +1,3 @@
-// src/features/todos/todoSlice.js
 import { createSlice } from '@reduxjs/toolkit'
 
 const todoSlice = createSlice({
@@ -6,17 +5,22 @@ const todoSlice = createSlice({
   initialState: [],
   reducers: {
     addTodo: (state, action) => {
-      state.push({ id: Date.now(), text: action.payload, completed: false })
+      state.push({ id: Date.now(), text: action.payload, completed: false });
     },
     toggleTodo: (state, action) => {
-      const todo = state.find(todo => todo.id === action.payload)
-      if (todo) todo.completed = !todo.completed
+      const todo = state.find(todo => todo.id === action.payload);
+      if (todo) todo.completed = !todo.completed;
     },
     deleteTodo: (state, action) => {
-      return state.filter(todo => todo.id !== action.payload)
+      return state.filter(todo => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, newText } = action.payload;
+      const todo = state.find(todo => todo.id === id);
+      if (todo) todo.text = newText;
     },
   },
-})
+});
 
-export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions
-export default todoSlice.reducer
+export const { addTodo, toggleTodo, deleteTodo, updateTodo } = todoSlice.actions;
+export default todoSlice.reducer;
